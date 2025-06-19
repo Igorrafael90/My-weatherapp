c287c856b12dba83e753600dce81021e925ed1ed
Igorrafael90 <rafaeligor804@gmail.com>
2025-06-18 22:31:12 -0300

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <div>
+          <form action="">
+            <input type="text" />
+            <button></button>
+          </form>
+        </div>
+      </main>
+    </>
+  );
+}
